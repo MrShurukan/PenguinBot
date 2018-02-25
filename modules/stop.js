f98cbded8a54d4cdd8d4c7958ffc03e8c19b782b
Ilya Zavyalov <ilya.zavyalov0@gmail.com>
2018-02-25 15:17:04 +0300

    v1.2.1! Now !stop command tells you "Stopped" when you write it

--- modules/stop.js
+++ modules/stop.js
@@ -1,9 +1,24 @@
 const fs = require("fs");
 module.exports = (msg) => {
+
+  // Pick the language
+  if (fs.existsSync('LANG.TXT')) lang = fs.readFileSync('LANG.TXT', 'utf8').trim();
+  else lang = "ENGLISH";
+
+  // Load translation file
+  if (lang == "RUSSIAN") translation = requireUncached('../translationsRussian.json');
+  else translation = requireUncached('../translationsEnglish.json');
+
+  msg.reply(translation.stopped);
   msg.member.voiceChannel.leave();
   if (fs.existsSync("./json/PLAYLIST.JSON")) {
     let queue = JSON.parse(fs.readFileSync('./json/PLAYLIST.JSON', 'utf8'));
     queue.forEach(x => x.currentlyPlaying = false);
     fs.writeFileSync('./json/PLAYLIST.JSON', JSON.stringify(queue), 'utf8');
   }
+}
+
+function requireUncached(module) {
+    delete require.cache[require.resolve(module)]
+    return require(module)
 }
